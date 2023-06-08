25410734d896a0ab67cdbef907646f2100fef0df
Rayhanendra <mrayhann@gmail.com>
2023-06-09 00:10:31 +0700

    refactor: improve ui

--- src/core/components/BaseButton.vue
+++ src/core/components/BaseButton.vue
@@ -10,12 +10,14 @@
 .button
     width 100%
     padding 20px 0
+    border 1px solid rgba(255, 255, 255, 0.2)
+    box-shadow 3px 5px 10px rgba(255, 138, 0, 0.2)
     background-color #FF8A00
+    border-radius 2px
     color #FFFFFF
     font-family 'Inter UI'
     font-size 16px
     font-weight bold
-    border none
     cursor pointer
     transition all 0.3s ease-in-out
 
--- src/core/views/HomeView.vue
+++ src/core/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <button @click="handleAddToCart">Add to Cart</button>
+  <div class="product">
+    <div class="product__card">
+      <img
+        class="product__image"
+        src="https://images.samsung.com/id/smartphones/galaxy-s21/buy/s21_group_kv_mo_img.jpg"
+        alt="product"
+      />
+      <div class="product__name">{{ item.name }}</div>
+      <div class="product__price">{{ $filters.formatCurrency(item.price) }}</div>
+      <div class="product__quantity">Jumlah: {{ item.quantity }}</div>
+      <button class="product__button" type="button" @click="handleAddToCart">Checkout</button>
+    </div>
   </div>
 </template>
 
@@ -15,7 +25,7 @@ const item = ref({
   name: 'Samsung Galaxy S21',
   slug: 'samsung-galaxy-s21',
   price: 1000,
-  quantity: 1
+  quantity: 50
 })
 
 const handleAddToCart = () => {
@@ -26,4 +36,58 @@ const handleAddToCart = () => {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="stylus">
+.product
+  height 100vh
+  display flex
+  justify-content center
+  align-items center
+
+  &__card
+    display flex
+    flex-direction column
+    align-items start
+    background-color #FFFFFF
+    box-shadow 2px 10px 20px rgba(255, 138, 0, 0.1);
+    border-radius 4px;
+    padding 20px
+    text-align center
+
+    &:hover
+      box-shadow 2px 10px 20px rgba(255, 138, 0, 0.2);
+
+  &__image
+    width 200px
+    height 200px
+    margin-bottom 10px
+
+  &__name
+    font-size 16px
+    font-weight 600
+    margin-bottom 10px
+
+  &__price
+    font-size 18px
+    font-weight 600
+    margin-bottom 10px
+
+  &__quantity
+    font-size 12px
+    margin-bottom 10px
+
+  &__button
+    width 100%
+    font-size 14px
+    font-weight 600
+    color #FF8A00
+    border 1px solid #FF8A00
+    border-radius 4px
+    padding 10px 20px
+    background-color transparent
+    cursor pointer
+    transition all 0.3s ease-in-out
+
+    &:hover
+      background-color #FF8A00
+      color #FFFFFF
+</style>
